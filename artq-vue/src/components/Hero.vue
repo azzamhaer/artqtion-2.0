d405a0955829855305321758e5200bb4b06c1bee
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-08 13:17:35 +0700

    vue

---
+++ artq-vue/src/components/Hero.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-full h-full bg-red-400">
+    <h1>artqtion</h1>
+    <h1>three.zero</h1>
+  </div>
+</template>
---
+++ artq-vue/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex p-3 w-full fixed top-0 left-0 bg-slate-800 text-white">
+    <div>
+      <span class="uppercase">
+        Logo <br />
+        Here</span
+      >
+    </div>
+    <div class="flex grow justify-center">
+      <div
+        :class="{
+          'translate-x-0': $route.path === '/',
+          'translate-x-0': $route.path === '/',
+          'translate-x-0': $route.path === '/',
+          'translate-x-0': $route.path === '/',
+        }"
+        class="grid grid-cols-4 text-center items-center font-bold relative"
+      >
+        <div class="w-30 nav-line h-1 absolute -bottom-3 bg-slate-300 left-0"></div>
+        <RouterLink class="home" to="/">Home</RouterLink>
+        <RouterLink class="comp" to="/competition">Competition</RouterLink>
+        <RouterLink class="schedule" to="/schedule">Schedule</RouterLink>
+        <RouterLink class="about w-30" to="/about">About</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.router-link-active {
+  @apply text-slate-300;
+}
+</style>
