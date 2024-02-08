3e8100756e171a267485092e8482bacd8827b408
Encapioy <fidiansyahalarif@gmail.com>
2024-02-08 01:33:52 -0800

    tabs underline

--- artq-vue/src/components/TabsWrapper.vue
+++ artq-vue/src/components/TabsWrapper.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="w-full">
         <ul class="flex justify-end mb-10">
-            <li class="text-3xl cursor-pointer uppercase mr-5" v-for="title in tabTitles" :key="title"
-                @click="selectedTitle = title">
+            <li class="text-4xl font-semibold cursor-pointer uppercase mr-10" v-for="title in tabTitles" :key="title"
+                :class="{ selected: title == selectedTitle }" @click="selectedTitle = title">
                 {{ title }}
             </li>
         </ul>
+        <div :class="{ 'w-16 right-[14.30rem]': selectedTitle === tabTitles[0], 'w-48 right-7': selectedTitle === tabTitles[1] }"
+            class="duration-300 text-slate-300 h-1 absolute -bottom-16 bg-gold-500"></div>
         <slot></slot>
     </div>
 </template>
@@ -13,7 +15,7 @@
 <script>
 import { ref, provide } from 'vue'
 export default {
-    setup(prps, { slots }) {
+    setup(props, { slots }) {
         const tabTitles = ref(slots.default().map((tab) => tab.props.title))
         const selectedTitle = ref(tabTitles.value[0])
 
