5a1b7f57ad9efbb601a716de5040eb605514bd73
Encapioy <fidiansyahalarif@gmail.com>
2024-02-07 23:56:03 -0800

    tabs

--- artq-vue/src/components/Competition.vue
+++ artq-vue/src/components/Competition.vue
@@ -1,27 +1,49 @@
 <template>
-    <div>
-        <h1></h1>
-    </div>
-
-
-
-
-    <div class="grid grid-cols-3 gap-28 w-full h-full">
-        <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-            <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-        </div>
-        <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-            <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-        </div>
-        <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-            <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-        </div>
-        <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-            <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-        </div>
-    </div>
+    <TabsWrapper>
+        <Tab title="SI">
+            <div class="grid grid-cols-3 gap-28 w-full h-full">
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                </div>
+            </div>
+        </Tab>
+        <Tab title="D'action">
+            <div class="grid grid-cols-3 gap-28 w-full h-full">
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                </div>
+                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                </div>
+            </div>
+        </Tab>
+    </TabsWrapper>
 </template>
+
+<script setup>
+import TabsWrapper from "./TabsWrapper.vue";
+import Tab from "./Tab.vue";
+</script>
---
+++ artq-vue/src/components/TabsWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-full">
+        <ul class="flex justify-end mb-10">
+            <li class="text-3xl cursor-pointer uppercase mr-5" v-for="title in tabTitles" :key="title"
+                @click="selectedTitle = title">
+                {{ title }}
+            </li>
+        </ul>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import { ref, provide } from 'vue'
+export default {
+    setup(prps, { slots }) {
+        const tabTitles = ref(slots.default().map((tab) => tab.props.title))
+        const selectedTitle = ref(tabTitles.value[0])
+
+        provide('selectedTitle', selectedTitle)
+        return {
+            selectedTitle,
+            tabTitles,
+        }
+    },
+}
+</script>
