2e3e11eafb65b2a3ed54c402e6414dcc5affbe85
Encapioy <fidiansyahalarif@gmail.com>
2024-02-08 01:42:49 -0800

    padding

--- artq-vue/src/components/Competition.vue
+++ artq-vue/src/components/Competition.vue
@@ -1,42 +1,46 @@
 <template>
     <TabsWrapper>
         <Tab title="SI">
-            <div class="grid grid-cols-4 gap-4 w-full h-full">
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+            <div class="px-10">
+                <div class="grid grid-cols-4 gap-4 w-full h-full">
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">art expression</h1>
+                    </div>
                 </div>
             </div>
         </Tab>
         <Tab title="D'action">
-            <div class="grid grid-cols-4 gap-4 w-full h-full">
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
-                </div>
-                <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
-                    <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                    <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+            <div class="px-10">
+                <div class="grid grid-cols-4 gap-4 w-full h-full">
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                    </div>
+                    <div class="relative h-96 rounded-lg bg-gray-600 pb-2 flex overflow-hidden">
+                        <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
+                        <h1 class="absolute text-5xl uppercase text-center text-white self-end mb-2">stand up comedy</h1>
+                    </div>
                 </div>
             </div>
         </Tab>
