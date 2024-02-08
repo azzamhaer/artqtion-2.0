11c7d3d8827554bdb0380c0653eb4cb5550bddc1
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-08 13:54:50 +0700

    assets

--- artq-vue/src/components/Navbar.vue
+++ artq-vue/src/components/Navbar.vue
@@ -15,12 +15,12 @@
             'translate-x-[200%]': $route.path === '/schedule',
             'translate-x-[300%]': $route.path === '/about',
           }"
-          class="duration-300 w-28 text-slate-300 nav-line h-1 absolute -bottom-3 bg-slate-300 left-0"
+          class="duration-300 w-28 text-slate-300 nav-line h-1 absolute -bottom-3 bg-gold-400 left-0"
         ></div>
-        <RouterLink class="home" to="/">Home</RouterLink>
-        <RouterLink class="comp" to="/competition">Competition</RouterLink>
-        <RouterLink class="schedule" to="/schedule">Schedule</RouterLink>
-        <RouterLink class="about w-28" to="/about">About</RouterLink>
+        <RouterLink class=" " to="/">Home</RouterLink>
+        <RouterLink class=" " to="/competition">Competition</RouterLink>
+        <RouterLink class=" " to="/schedule">Schedule</RouterLink>
+        <RouterLink class="w-28" to="/about">About</RouterLink>
       </div>
     </div>
   </div>
