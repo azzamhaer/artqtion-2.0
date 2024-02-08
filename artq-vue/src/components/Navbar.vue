f6c52920a5631cb565535c68c6a45003bd769196
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-08 13:28:08 +0700

    navbar

--- artq-vue/src/components/Navbar.vue
+++ artq-vue/src/components/Navbar.vue
@@ -7,27 +7,23 @@
       >
     </div>
     <div class="flex grow justify-center">
-      <div
-        :class="{
-          'translate-x-0': $route.path === '/',
-          'translate-x-0': $route.path === '/',
-          'translate-x-0': $route.path === '/',
-          'translate-x-0': $route.path === '/',
-        }"
-        class="grid grid-cols-4 text-center items-center font-bold relative"
-      >
-        <div class="w-30 nav-line h-1 absolute -bottom-3 bg-slate-300 left-0"></div>
+      <div class="grid grid-cols-4 text-center items-center font-bold relative">
+        <div
+          :class="{
+            'translate-x-0': $route.path === '/',
+            'translate-x-[100%]': $route.path === '/competition',
+            'translate-x-[200%]': $route.path === '/schedule',
+            'translate-x-[300%]': $route.path === '/about',
+          }"
+          class="duration-300 w-28 text-slate-300 nav-line h-1 absolute -bottom-3 bg-slate-300 left-0"
+        ></div>
         <RouterLink class="home" to="/">Home</RouterLink>
         <RouterLink class="comp" to="/competition">Competition</RouterLink>
         <RouterLink class="schedule" to="/schedule">Schedule</RouterLink>
-        <RouterLink class="about w-30" to="/about">About</RouterLink>
+        <RouterLink class="about w-28" to="/about">About</RouterLink>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-.router-link-active {
-  @apply text-slate-300;
-}
-</style>
+<style></style>
