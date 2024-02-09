e5f808e5133ea54eea90dea173082df2dcde1d4e
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-09 20:43:33 +0700

    au deh

--- artq-vue/src/components/Schedule.vue
+++ artq-vue/src/components/Schedule.vue
@@ -1,171 +1,143 @@
 <template>
-<<<<<<< HEAD
-    <div class="py-20">
-        <TabsWrapperTl>
-            <Tab title="Day 1">
-                <div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Gladi Kotor</h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Gladi Resik</h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Grand Opening</h2>
-                        </div>
-                    </div>
-                </div>
-            </Tab>
-            <Tab title="Day 2">
-                <div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Penyambutan</h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Kanvas Sejarah</h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Debate</h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Penutupan</h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                    </div>
-                </div>
-            </Tab>
-            <Tab title="Online">
-                <div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Pendaftaran</h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Pengumpulan akhir</h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                    </div>
-                    <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
-                        <!-- Timeline Component-->
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
-                            <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
-                        </div>
-                        <div class="relative bg-gold-500">
-                            <div
-                                class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]">
-                            </div>
-                        </div>
-                        <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
-                            <h2 class="mb-[10px] text-[1.2em] font-semibold">Grand Closing</h2>
-                        </div>
-                    </div>
-                </div>
-            </Tab>
-        </TabsWrapperTl>
-    </div>
+  <div class="py-20">
+    <TabsWrapperTl>
+      <Tab title="Day 1">
+        <div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Gladi Kotor</h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Gladi Resik</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Grand Opening</h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+      <Tab title="Day 2">
+        <div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Penyambutan</h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Kanvas Sejarah</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Debate</h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Penutupan</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+      <Tab title="Online">
+        <div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Pendaftaran</h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold">Pengumpulan akhir</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+          </div>
+          <div class="max-w-full px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 mr-5">
+              <h2 class="text-right mb-[10px] text-[1.2em] font-semibold"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-8 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 mx-5 mb-5 block p-[1.5em] text-gold-500 -mr-5">
+              <h2 class="mb-[10px] text-[1.2em] font-semibold">Grand Closing</h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+    </TabsWrapperTl>
+  </div>
 </template>
 
 <script setup>
 import TabsWrapperTl from "./TabsWrapperTl.vue";
 import Tab from "./Tab.vue";
-
 </script>
-=======
-  <div class="w-full justify-center flex min-h-screen items-center">
-    <div class="container">ini schedule</div>
-  </div>
-</template>
->>>>>>> 25262195084ca5ff833b22f87d680167ccb80197
