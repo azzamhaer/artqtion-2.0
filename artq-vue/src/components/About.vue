cff716563a12513ef08e4a3e3120c78222b8e60e
Encapioy <fidiansyahalarif@gmail.com>
2024-02-08 19:04:59 -0800

    about

--- artq-vue/src/components/About.vue
+++ artq-vue/src/components/About.vue
@@ -1,1 +1,19 @@
-<template>ini abouts</template>
+<template>
+    <div class="py-20 px-10 h-screen">
+        <div class="grid grid-cols-3 h-80">
+            <div class="h-full">
+                <img class="h-full object-cover rounded-bl-[3.5rem] rounded-tr-[3.5rem] rounded-tl-xl rounded-br-xl"
+                    src="../assets/img/bg/bg-1.jpg" alt="">
+            </div>
+            <div class="col-span-2 ml-20 py-5">
+                <h2 class="text-5xl uppercase font-semibold">About</h2>
+                <div class="duration-300 text-slate-300 w-48 h-0.5 mt-3 mb-4 bg-gold-500"></div>
+                <p class="text-lg mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nemo animi? Nostrum
+                    error
+                    molestias quasi
+                    iste numquam ab, magnam veniam culpa odit non, vero hic, recusandae harum suscipit fugiat reprehenderit?
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
--- artq-vue/src/components/Competition.vue
+++ artq-vue/src/components/Competition.vue
@@ -4,29 +4,11 @@
             <Tab title="SI">
                 <div class="px-10">
                     <div class="grid grid-cols-3 gap-28 w-full h-full">
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                art
-                                expression</h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                art
-                                expression</h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                art
-                                expression</h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                art
-                                expression</h1>
+                        <div v-for="item in intCompetition" :key="item.nama"
+                            class="relative h-96 rounded-lg bg-gray-900 pb-2 flex justify-center overflow-hidden">
+                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg/bg-1.jpg" alt="">
+                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mb-5"
+                                v-text="item.nama"></h1>
                         </div>
                     </div>
                 </div>
@@ -34,33 +16,11 @@
             <Tab title="D'action">
                 <div class="px-10">
                     <div class="grid grid-cols-3 gap-28 w-full h-full">
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                stand
-                                up comedy
-                            </h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                stand
-                                up comedy
-                            </h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                stand
-                                up comedy
-                            </h1>
-                        </div>
-                        <div class="relative h-96 rounded-lg bg-gray-900 pb-2 flex overflow-hidden">
-                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg-1.jpg" alt="">
-                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mx-5 mb-3">
-                                stand
-                                up comedy
-                            </h1>
+                        <div v-for="item in extCompetition" :key="item.nama"
+                            class="relative h-96 rounded-lg bg-gray-900 pb-2 flex justify-center overflow-hidden">
+                            <img class="object-cover brightness-50 rounded-lg" src="../assets/img/bg/bg-1.jpg" alt="">
+                            <h1 class="absolute text-4xl font-semibold uppercase text-center text-white self-end mb-5"
+                                v-text="item.nama"></h1>
                         </div>
                     </div>
                 </div>
@@ -72,4 +32,31 @@
 <script setup>
 import TabsWrapper from "./TabsWrapper.vue";
 import Tab from "./Tab.vue";
+import { ref } from "vue";
+
+const intCompetition = ref([
+    {
+        nama: "Art Expression"
+    },
+    {
+        nama: "Stand Up Comedy"
+    },
+    {
+        nama: "Water Color Punch"
+    },
+])
+
+const extCompetition = ref([
+    {
+        nama: "Gartic io"
+    },
+    {
+        nama: "Debate"
+    },
+    {
+        nama: "WSIB"
+    },
+])
+
+
 </script>
