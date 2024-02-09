7fe0e628845e619495aab897d1e21d57eaed0afe
Encapioy <fidiansyahalarif@gmail.com>
2024-02-08 19:35:15 -0800

    logo n btn

--- artq-vue/src/components/About.vue
+++ artq-vue/src/components/About.vue
@@ -6,13 +6,35 @@
                     src="../assets/img/bg/bg-1.jpg" alt="">
             </div>
             <div class="col-span-2 ml-20 py-5">
-                <h2 class="text-5xl uppercase font-semibold">About</h2>
-                <div class="duration-300 text-slate-300 w-48 h-0.5 mt-3 mb-4 bg-gold-500"></div>
-                <p class="text-lg mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nemo animi? Nostrum
-                    error
-                    molestias quasi
-                    iste numquam ab, magnam veniam culpa odit non, vero hic, recusandae harum suscipit fugiat reprehenderit?
-                </p>
+                <div>
+                    <h2 class="text-5xl uppercase font-semibold">About</h2>
+                    <div class="duration-300 text-slate-300 w-48 h-0.5 mt-3 mb-4 bg-gold-500"></div>
+                    <p class="text-lg mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nemo animi?
+                        Nostrum
+                        error
+                        molestias quasi
+                        iste numquam ab, magnam veniam culpa odit non, vero hic, recusandae harum suscipit fugiat
+                        reprehenderit?
+                    </p>
+                </div>
+                <div class="w-64 flex justify-between">
+                    <button class="bg-gradient-to-r from-sky-900 p-2 rounded-full"><a
+                            class="text-lg font-semibold capitalize" href="">our
+                            youtube</a></button>
+                    <button class="bg-gradient-to-r from-sky-900 p-2 rounded-full"><a
+                            class="text-lg font-semibold capitalize" href="">our
+                            youtube</a></button>
+                </div>
+                <div class="flex mt-8">
+                    <div class="h-28 mx-5">
+                        <img class="h-full object-cover" src="../assets/img/logo-os.png" alt="">
+                    </div>
+                    <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
+                    <div class="h-28 mx-5">
+                        <img class="h-full object-cover" src="../assets/img/logo-si.png" alt="">
+                    </div>
+                    <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
+                </div>
             </div>
         </div>
     </div>
