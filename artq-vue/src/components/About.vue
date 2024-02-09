3ea57671b0dc8e4016631123e91bc34628cfab09
Encapioy <fidiansyahalarif@gmail.com>
2024-02-09 06:26:13 -0800

    schedule lg

--- artq-vue/src/components/About.vue
+++ artq-vue/src/components/About.vue
@@ -1,39 +1,49 @@
 <template>
-  <div class="py-20 px-10 min-h-screen flex items-center justify-center">
-    <div class="grid container grid-cols-3">
-      <div class="h-full">
-        <img class="h-full object-cover rounded-bl-[3.5rem] rounded-tr-[3.5rem] rounded-tl-xl rounded-br-xl" src="../assets/img/bg/bg-1.jpg" alt="" />
-      </div>
-      <div class="col-span-2 ml-20 py-5">
-        <div>
-          <h2 class="text-5xl uppercase font-semibold">About</h2>
-          <div class="duration-300 text-slate-300 w-48 h-0.5 mt-3 mb-4 bg-gold-500"></div>
-          <p class="text-lg mb-2">
-            Art, Quranic, Technology, dan Competition atau ArtQTion adalah kegiatan tahunan yang di adakan oleh Santri Sekolah Impian sejak tahun 2022. <br />
-            <br />
-            ArtQTion adalah acara edukasi yang bertemakan "Back to Golden Age of Islam to fight AI Disruption". Acara ini bertujuan untuk mengedukasi santri sekaligus siswa/siswi tentang bagaimana masyarakat menanggapi disrupsi AI dewasa
-            ini. Terkadang, perkembangan teknologi yang amat masif membuat masyarakat tertinggal dan tidak tahu untuk menanggapinya. Sehingga acara ini dihadirkan untuk membuka wawasan dan pemahaman bahwa AI dapat menjadi alat yang
-            bermanfaat dan positif dalam memajukan kehidupan manusia dan tentunya Umat Islam.
-            <br /><br />
-            Melalui pendekatan inovatif, ArtQTion mengajak para peserta untuk berpikir kreatif dan mengembangkan visi yang jauh ke depan tentang teknologi dalam Islam. Dalam acara ini, akan disiapkan kompetisi-kompetisi menarik untuk
-            memberikan inspirasi dan pencerahan kepada para peserta.
-          </p>
+    <div class="py-20 lg:px-10 min-h-screen flex items-center justify-center">
+        <div class="grid container grid-cols-1 lg:grid-cols-3">
+            <div class="h-full">
+                <img class="h-full object-cover rounded-bl-[3.5rem] rounded-tr-[3.5rem] rounded-tl-xl rounded-br-xl"
+                    src="../assets/img/bg/bg-1.jpg" alt="" />
+            </div>
+            <div class="col-span-2 ml-20 py-5">
+                <div>
+                    <h2 class="text-5xl uppercase font-semibold">About</h2>
+                    <div class="duration-300 text-slate-300 w-48 h-0.5 mt-3 mb-4 bg-gold-500"></div>
+                    <p class="text-lg mb-10">
+                        Art, Quranic, Technology, dan Competition atau ArtQTion adalah kegiatan tahunan yang di adakan oleh
+                        Santri Sekolah Impian sejak tahun 2022. <br />
+                        <br />
+                        ArtQTion adalah acara edukasi yang bertemakan "Back to Golden Age of Islam to fight AI Disruption".
+                        Acara ini bertujuan untuk mengedukasi santri sekaligus siswa/siswi tentang bagaimana masyarakat
+                        menanggapi disrupsi AI dewasa
+                        ini. Terkadang, perkembangan teknologi yang amat masif membuat masyarakat tertinggal dan tidak tahu
+                        untuk menanggapinya. Sehingga acara ini dihadirkan untuk membuka wawasan dan pemahaman bahwa AI
+                        dapat menjadi alat yang
+                        bermanfaat dan positif dalam memajukan kehidupan manusia dan tentunya Umat Islam.
+                        <br /><br />
+                        Melalui pendekatan inovatif, ArtQTion mengajak para peserta untuk berpikir kreatif dan mengembangkan
+                        visi yang jauh ke depan tentang teknologi dalam Islam. Dalam acara ini, akan disiapkan
+                        kompetisi-kompetisi menarik untuk
+                        memberikan inspirasi dan pencerahan kepada para peserta.
+                    </p>
+                </div>
+                <div class="w-64 flex justify-between">
+                    <button class="bg-gradient-to-r from-gold-400 p-2 rounded-full"><a
+                            class="text-lg font-semibold capitalize" href="">our youtube</a></button>
+                    <button class="bg-gradient-to-r from-gold-400 p-2 rounded-full"><a
+                            class="text-lg font-semibold capitalize" href="">our youtube</a></button>
+                </div>
+                <div class="flex mt-8">
+                    <div class="h-28 mx-5">
+                        <img class="h-full object-cover" src="../assets/img/logo-os.png" alt="" />
+                    </div>
+                    <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
+                    <div class="h-28 mx-5">
+                        <img class="h-full object-cover" src="../assets/img/logo-si.png" alt="" />
+                    </div>
+                    <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
+                </div>
+            </div>
         </div>
-        <div class="w-64 flex justify-between">
-          <button class="bg-gradient-to-r from-gold-400 p-2 rounded-full"><a class="text-lg font-semibold capitalize" href="">our youtube</a></button>
-          <button class="bg-gradient-to-r from-gold-400 p-2 rounded-full"><a class="text-lg font-semibold capitalize" href="">our youtube</a></button>
-        </div>
-        <div class="flex mt-8">
-          <div class="h-28 mx-5">
-            <img class="h-full object-cover" src="../assets/img/logo-os.png" alt="" />
-          </div>
-          <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
-          <div class="h-28 mx-5">
-            <img class="h-full object-cover" src="../assets/img/logo-si.png" alt="" />
-          </div>
-          <div class="duration-300 text-slate-300 w-px mx-5 bg-gold-500"></div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
--- artq-vue/src/components/Navbar.vue
+++ artq-vue/src/components/Navbar.vue
@@ -3,24 +3,20 @@
     <div>
       <span class="uppercase">
         Logo <br />
-        Here</span
-      >
+        Here</span>
     </div>
     <div class="flex grow justify-center">
       <div class="grid grid-cols-4 text-center items-center font-bold relative">
-        <div
-          :class="{
-            'translate-x-0': $route.path === '/',
-            'translate-x-[100%]': $route.path === '/competition',
-            'translate-x-[200%]': $route.path === '/schedule',
-            'translate-x-[300%]': $route.path === '/about',
-          }"
-          class="duration-300 w-28 text-slate-300 nav-line h-1 absolute -bottom-3 bg-gold-500 left-0"
-        ></div>
+        <div :class="{
+          'translate-x-0': $route.path === '/',
+          'translate-x-[100%]': $route.path === '/about',
+          'translate-x-[200%]': $route.path === '/competition',
+          'translate-x-[300%]': $route.path === '/schedule',
+        }" class="duration-300 w-28 text-slate-300 nav-line h-1 absolute -bottom-3 bg-gold-500 left-0"></div>
         <RouterLink class="nav-link" to="/">Home</RouterLink>
+        <RouterLink class="nav-link w-28" to="/about">About</RouterLink>
         <RouterLink class="nav-link" to="/competition">Competition</RouterLink>
         <RouterLink class="nav-link" to="/schedule">Schedule</RouterLink>
-        <RouterLink class="nav-link w-28" to="/about">About</RouterLink>
       </div>
     </div>
   </div>
