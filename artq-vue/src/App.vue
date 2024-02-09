56a1243e46fae8383c822ac6348d7b7ecb4cb822
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-09 10:55:23 +0700

    bg

--- artq-vue/src/App.vue
+++ artq-vue/src/App.vue
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar.vue";
 <template>
   <Navbar />
   <div class="bg-[url('./assets/img/bg/bg-2.png')] bg-fixed bg-cover w-screen h-screen fixed -z-50 top-0 left-0 brightness-90">
-    <div class="w-screen h-screen bg-black opacity-25"></div>
+    <div class="w-screen h-screen bg-black opacity-50"></div>
   </div>
   <div class="text-white">
     <RouterView />
