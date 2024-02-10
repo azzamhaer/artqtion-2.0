90a8f5aeda37dd08a889d4a4073591fc4edc14c8
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-10 16:06:30 +0700

    Revert "akhirnyaaa responsif"
    This reverts commit dc5f11272cc71fc11893421f9048d55f69568669.

---
+++ artq-vue/src/components/Schedule.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="py-20">
+    <TabsWrapperTl>
+      <Tab title="Day 1">
+        <div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Gladi Kotor</h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Gladi Resik</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Grand Opening</h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+      <Tab title="Day 2">
+        <div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Penyam<br />butan</h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Kanvas Sejarah</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Debate</h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Penutupan</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="mt-0 lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase"></h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+      <Tab title="Online">
+        <div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="lg:mx-5 mb-5 block p-[1.5em] text-white mt-[5.5rem] mr-5">
+              <h2 class="text-right text-[8px] lg:text-xs font-bold uppercase">10 - 17 Februari 2024</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Pendaf<br />taran</h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:mr-5">
+              <h2 class="text-right mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Pengum<br />pulan akhir</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="lg:mx-5 mb-5 block p-[1.5em] text-white mt-[5.5rem] lg:-mr-5">
+              <h2 class="text-[8px] lg:text-xs font-bold uppercase">24 Februari 2024</h2>
+            </div>
+          </div>
+          <div class="max-w-full h-80 px-[25px] grid grid-cols-[1fr_1px_1fr]">
+            <!-- Timeline Component-->
+            <div class="lg:mx-5 block p-[1.5em] text-white mt-[5.5rem] mr-5">
+              <h2 class="text-right text-[8px] lg:text-xs font-bold uppercase">1 maret 2024</h2>
+            </div>
+            <div class="relative bg-gold-500">
+              <div class="absolute top-28 left-1/2 -translate-x-1/2 w-[15px] h-[15px] bg-gold-500 rounded-[50%]"></div>
+            </div>
+            <div class="lg:mx-5 mb-5 block p-[1.5em] text-gold-500 mt-20 lg:-mr-5">
+              <h2 class="mb-[10px] text-sm lg:text-[1.2em] font-bold uppercase">Grand Closing</h2>
+            </div>
+          </div>
+        </div>
+      </Tab>
+    </TabsWrapperTl>
+  </div>
+</template>
+
+<script setup>
+import TabsWrapperTl from "./TabsWrapperTl.vue";
+import Tab from "./Tab.vue";
+</script>
---
+++ artq-vue/src/components/TabsWrapperTl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative w-full">
+    <ul class="flex justify-end mb-10">
+      <li class="text-xl lg:text-4xl font-semibold cursor-pointer uppercase mr-10" v-for="title in tabTitles" :key="title" :class="{ selected: title == selectedTitle }" @click="selectedTitle = title">
+        {{ title }}
+      </li>
+    </ul>
+    <div
+      :class="{ 'w-20 lg:w-32 right-[14.5rem] lg:right-[20.5rem]': selectedTitle === tabTitles[0], 'w-20 lg:w-32 right-[8.5rem] lg:right-48': selectedTitle === tabTitles[1], 'w-24 lg:w-36 right-7': selectedTitle === tabTitles[2] }"
+      class="duration-300 text-slate-300 h-0.5 absolute top-12 bg-gold-500"
+    ></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { ref, provide } from "vue";
+export default {
+  setup(props, { slots }) {
+    const tabTitles = ref(slots.default().map((tab) => tab.props.title));
+    const selectedTitle = ref(tabTitles.value[0]);
+
+    provide("selectedTitle", selectedTitle);
+    return {
+      selectedTitle,
+      tabTitles,
+    };
+  },
+};
+</script>
