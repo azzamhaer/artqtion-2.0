fcfd4a7e4e3416bba3e8b60acbfb0b935ecacba5
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-10 16:13:49 +0700

    Revert "akhirnyaaa responsif"
    This reverts commit dc5f11272cc71fc11893421f9048d55f69568669.

--- artq-vue/src/components/Competition.vue
+++ artq-vue/src/components/Competition.vue
@@ -4,9 +4,13 @@
       <Tab v-for="event in events" :key="event.id" :title="event.name">
         <div class="w-full flex">
           <div class="flex gap-5 p-3 overflow-x-scroll container">
-            <div @click="openModal(competition)" :class="event.id === 2 ? 'min-w-[49%]' : 'min-w-[20rem]'"
-              class="bg-gold-900 bg-opacity-65 h-96 rounded-xl cursor-pointer group"
-              v-for="competition in event.competitions" :key="competition.id">
+            <div
+              @click="openModal(competition)"
+              :class="event.id === 2 ? 'min-w-[49%]' : 'min-w-[20rem]'"
+              class="bg-gold-900 bg-opacity-90 h-96 rounded-xl cursor-pointer group"
+              v-for="competition in event.competitions"
+              :key="competition.id"
+            >
               <div class="h-1/2 p-5 uppercase flex flex-row-reverse">
                 <img class="w-auto h-full" :src="competition.imgUrl" alt="" />
               </div>
