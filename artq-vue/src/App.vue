d4e2e5a2574e09f1b65fd4338a81e54920da38f7
Encapioy <fidiansyahalarif@gmail.com>
2024-02-09 19:53:44 -0800

    ssa

--- artq-vue/src/App.vue
+++ artq-vue/src/App.vue
@@ -9,14 +9,9 @@ const modal = useModalStore();
 
 <template>
   <Navbar />
-<<<<<<< HEAD
   <Modal v-if="modal.modalState" />
   <div
     class="bg-[url('./assets/img/bg/bg-2.png')] bg-fixed bg-cover w-screen h-screen fixed -z-50 top-0 left-0 brightness-90">
-=======
-  <Modal />
-  <div class="bg-[url('./assets/img/bg/bg-2.png')] bg-fixed bg-cover w-screen h-screen fixed -z-50 top-0 left-0 brightness-90">
->>>>>>> dc5f11272cc71fc11893421f9048d55f69568669
     <div class="w-screen h-screen bg-black opacity-65"></div>
   </div>
   <div class="text-white min-h-screen items-center flex flex-col p-5 gap-5 justify-center">
