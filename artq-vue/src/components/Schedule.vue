bb88cf8a2b8bca96636e1249d184ee85bcb4d7ce
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-02-11 22:26:35 +0700

    akhirnyaaaaaaaaaaaa (rill coooy)

--- artq-vue/src/components/Schedule.vue
+++ artq-vue/src/components/Schedule.vue
@@ -2,7 +2,7 @@
   <div class="py-20 container">
     <TabsWrapperTl :tabTitles="modal.daysTitles">
       <Tab class="flex justify-center" :title="day.title" v-for="day in modal.days" :key="day.id">
-        <div class="w-3/4 flex justify-center bg-gradient-to-bl from-blue-500 to-blue-900 rounded-3xl py-5" :style="`height: ${day.schedules.length * 20}rem`">
+        <div class="w-3/4 flex justify-center bg-gradient-to-bl from-blue-600 to-blue-900 rounded-3xl py-5" :style="`height: ${day.schedules.length * 20}rem`">
           <div class="w-1 h-full bg-gold-700 rounded-full flex flex-col justify-around items-center">
             <div v-for="(schedule, index) in day.schedules" :key="schedule.id" class="w-4 h-4 bg-gold-900 capitalize border-white border rounded-full relative">
               <div class="w-3 h-3 bg-gold-900 border-white border rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
--- artq-vue/src/components/TabsWrapperTl.vue
+++ artq-vue/src/components/TabsWrapperTl.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative w-full flex flex-col gap-5">
-    <div class="grid gap-y-4 text-center relative" :class="`grid-cols-${tabTitles.length}`">
+  <div class="relative w-full justify-center flex flex-col gap-12">
+    <div class="grid gap-y-4 py-4 w-3/5 self-center text-center relative mb-10" :class="`grid-cols-${tabTitles.length}`">
       <span v-for="(tabTitle, index) in tabTitles" :key="tabTitle" :class="{ selected: tabTitle === selectedTitle }" @click="selectedTitle = tabTitle" class="text-2xl px-3 text-center lg:text-4xl font-semibold cursor-pointer uppercase">
         {{ tabTitle }}
       </span>
-      <div :style="{ transform: `translateX(${tabTitles.indexOf(selectedTitle)}00%)` }" class="duration-300 left-0 text-slate-300 h-0.5 absolute w-1/3 -bottom-2 bg-gold-500"></div>
+      <div :style="{ transform: `translateX(${tabTitles.indexOf(selectedTitle)}00%)` }" class="duration-300 left-0 text-slate-300 h-1 absolute w-1/3 -bottom-2 bg-blue-500"></div>
     </div>
     <slot></slot>
   </div>
